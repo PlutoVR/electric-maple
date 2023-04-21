d323fedae38a6726d0478bbcea28dc1b58976b79
Moshi Turner <moses@collabora.com>
2023-04-21 17:45:46 -0500

    have everything use 3.20.0, kinda works

---
+++ client/download_gst.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Go to where the script is
+cd $(dirname $0)
+
+# Create the destination directory if it doesn't exist
+mkdir -p ./deps/gstreamer_android
+
+# Download the file using curl
+curl -L -o ./deps/gstreamer_android/gstreamer-1.0-android-universal-1.22.2.tar.xz https://gstreamer.freedesktop.org/data/pkg/android/1.22.2/gstreamer-1.0-android-universal-1.22.2.tar.xz
+
+# Extract the contents of the downloaded file to the destination directory
+tar -xf ./deps/gstreamer_android/gstreamer-1.0-android-universal-1.22.2.tar.xz -C ./deps/gstreamer_android
+
+rm ./deps/gstreamer_android/gstreamer-1.0-android-universal-1.22.2.tar.xz
