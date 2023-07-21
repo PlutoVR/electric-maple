08307b2e2acddc82f07e5dc8512215e7649fa0cc
Ryan Pavlik <ryan.pavlik@collabora.com>
2023-07-21 12:59:19 -0500

    Activity override

--- client/app/src/main/java/com/plutovr/plutosphere/webrtc_client/StreamingActivity.kt
+++ client/app/src/main/java/com/plutovr/plutosphere/webrtc_client/StreamingActivity.kt
@@ -1,16 +1,27 @@
 package com.plutovr.plutosphere.webrtc_client
+
 import org.freedesktop.gstreamer.GStreamer
 
 import android.app.NativeActivity
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.util.Log
 
-class StreamingActivity: NativeActivity() {
+class StreamingActivity : NativeActivity() {
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+        Log.i("ElectricMaple", "StreamingActivity: In onCreate")
         GStreamer.init(this)
+        super.onCreate(savedInstanceState, persistentState)
     }
-    init {
 
+    companion object {
+        init {
+            Log.i("ElectricMaple", "StreamingActivity: In StreamingActivity static init")
+
+            System.loadLibrary("pluto_vf")
+            System.loadLibrary("plutovr_webrtc_client")
+            Log.i("ElectricMaple", "StreamingActivity: Done with static init and System.loadLibrary")
+
+        }
     }
 }
