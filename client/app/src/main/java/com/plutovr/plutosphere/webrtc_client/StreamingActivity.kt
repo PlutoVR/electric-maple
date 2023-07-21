0dcba19294bb366482086719e710c350da73de3a
Rylie Pavlik <rylie.pavlik@collabora.com>
2023-07-21 12:59:19 -0500

    More work on activity

--- client/app/src/main/java/com/plutovr/plutosphere/webrtc_client/StreamingActivity.kt
+++ client/app/src/main/java/com/plutovr/plutosphere/webrtc_client/StreamingActivity.kt
@@ -1,16 +1,30 @@
 package com.plutovr.plutosphere.webrtc_client
+
 import org.freedesktop.gstreamer.GStreamer
 
 import android.app.NativeActivity
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.util.Log
 
-class StreamingActivity: NativeActivity() {
+class StreamingActivity : NativeActivity() {
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+        Log.i("ElectricMaple", "StreamingActivity: In onCreate")
+        System.loadLibrary("pluto_vf")
+        Log.i("ElectricMaple", "StreamingActivity: loaded pluto_vf")
+        Log.i("ElectricMaple", "StreamingActivity: Calling GStreamer.init")
         GStreamer.init(this)
+        Log.i("ElectricMaple", "StreamingActivity: Done with GStreamer.init")
+        System.loadLibrary("plutovr_webrtc_client")
+        Log.i("ElectricMaple", "StreamingActivity: loaded")
+        super.onCreate(savedInstanceState, persistentState)
     }
-    init {
 
+    companion object {
+        init {
+            Log.i("ElectricMaple", "StreamingActivity: In StreamingActivity static init")
+
+
+        }
     }
 }
