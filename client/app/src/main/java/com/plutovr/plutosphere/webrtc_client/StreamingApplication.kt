4708b08b7a17632340101f723837b156d782b252
Ryan Pavlik <ryan.pavlik@collabora.com>
2023-07-21 12:37:39 -0500

    try more gradle java stuff

--- client/app/src/main/java/com/plutovr/plutosphere/webrtc_client/StreamingApplication.kt
+++ client/app/src/main/java/com/plutovr/plutosphere/webrtc_client/StreamingApplication.kt
@@ -1,6 +1,10 @@
 import android.app.Application;
-class StreamingApplication :Application()  {
+import org.freedesktop.gstreamer.GStreamer
 
+class StreamingApplication :Application()  {
+    override fun onCreate() {
+        super.onCreate()
+    }
     init {
         System.loadLibrary("pluto_vf")
         System.loadLibrary("pluto_webrtc_runtime")
