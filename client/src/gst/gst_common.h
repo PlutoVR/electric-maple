570ab848e61760106f554dcc3193d64c5aa06d80
Rylie Pavlik <rylie.pavlik@collabora.com>
2023-07-24 18:00:36 -0500

    Progress in manually merging branches

--- client/src/gst/gst_common.h
+++ client/src/gst/gst_common.h
@@ -1,10 +1,13 @@
-// Copyright 2020-2021, Collabora, Ltd.
+// Copyright 2020-2023, Collabora, Ltd.
 // SPDX-License-Identifier: BSL-1.0
 /*!
  * @file
  * @brief  Header
+ * @author Rylie Pavlik <rpavlik@collabora.com>
+ * @author Moshi Turner <moses@collabora.com>
+ * @author Jakob Bornecrantz <jakob@collabora.com>
  * @author Christoph Haag <christoph.haag@collabora.com>
- * @ingroup drv_vf
+ * @ingroup xrt_fs_em
  */
 
 #pragma once
@@ -12,13 +15,8 @@
 #define XR_USE_PLATFORM_ANDROID
 #define XR_USE_GRAPHICS_API_OPENGL_ES
 
-#include "os/os_time.h"
-#include "os/os_threading.h"
-
 #include "xrt/xrt_frame.h"
-#include "util/u_logging.h"
-#include "util/u_time.h"
-#include "os/os_time.h"
+#include "os/os_threading.h"
 
 #include <android_native_app_glue.h>
 #include <GLES3/gl3.h>
@@ -27,17 +25,8 @@
 #include <android/log.h>
 #include <jni.h>
 
-#include <gst/gst.h>
-#include <gst/gl/gl.h>
-#include <gst/app/gstappsink.h>
-#include <gst/gstelement.h>
-#include <gst/video/video-frame.h>
-
 #include <openxr/openxr.h>
 #include <openxr/openxr_platform.h>
-
-#include "pluto.pb.h"
-#include "pb_encode.h"
 
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -49,7 +38,8 @@
 #define XR_LOAD(fn) xrGetInstanceProcAddr(state.instance, #fn, (PFN_xrVoidFunction *)&fn);
 
 #include "xrt/xrt_frameserver.h"
-#include "gstjniutils.h"
+
+struct em_fs;
 
 struct state_t
 {
@@ -58,14 +48,17 @@ struct state_t
 	JavaVM *java_vm;
 	bool hasPermissions;
 
-	struct vf_fs *vid;
+	struct em_fs *vid;
 
 	// This mutex protects the EGL context below across main and gstgl threads
 	struct os_mutex egl_lock;
 
 	EGLDisplay display;
+	// context created in initializeEGL
 	EGLContext context;
+	// config used to create context
 	EGLConfig config;
+	// 16x16 pbuffer surface
 	EGLSurface surface;
 	XrInstance instance;
 	XrSystemId system;
@@ -96,55 +89,6 @@ struct state_t
 	GLboolean frame_available;
 };
 
-// FIXME : MERGE VID AND STATE !!!! THIS IS SO UGLY !!
-struct vf_fs
-{
-	struct xrt_fs base;
-
-	struct os_thread_helper play_thread;
-
-	GMainLoop *loop;
-	GstElement *pipeline;
-	GstGLDisplay *gst_gl_display;
-	GstGLContext *gst_gl_context;
-	GstGLContext *gst_gl_other_context;
-
-	GstGLDisplay *display;
-	GstGLContext *other_context;
-	GstGLContext *context;
-	GstElement *appsink;
-	// GLenum texture_target; // WE SHOULD USE RENDER'S texture target
-	// GLuint texture_id; // WE SHOULD USE RENDER'S texture id
-
-	int width;
-	int height;
-	enum xrt_format format;
-	enum xrt_stereo_format stereo_format;
-
-	struct xrt_frame_node node;
-
-	struct
-	{
-		bool extended_format;
-		bool timeperframe;
-	} has;
-
-	enum xrt_fs_capture_type capture_type;
-	struct xrt_frame_sink *sink;
-
-	uint32_t selected;
-
-	struct xrt_fs_capture_parameters capture_params;
-
-	bool is_configured;
-	bool is_running;
-	enum u_logging_level log_level;
-
-	struct state_t *state;
-	JavaVM *java_vm;
-};
-
-
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -152,27 +96,12 @@ extern "C" {
 static const char *dotfilepath = "mrow";
 
 /*!
- * @defgroup drv_vf Video Fileframeserver driver
- * @ingroup drv
+ * Create an ElectricMaple XR streaming frameserver with default parameters.
  *
- * @brief Frameserver using a video file.
- */
-
-/*!
- * Create a vf frameserver by opening a video file.
- *
- * @ingroup drv_vf
+ * Must call from a thread in which we can safely make @p context active.
  */
 struct xrt_fs *
-vf_fs_open_file(struct xrt_frame_context *xfctx, const char *path);
-
-/*!
- * Create a vf frameserver that uses the videotestsource.
- *
- * @ingroup drv_vf
- */
-struct xrt_fs *
-vf_fs_gst_pipeline(struct xrt_frame_context *xfctx, struct state_t *state);
+em_fs_gst_pipeline(struct xrt_frame_context *xfctx, struct state_t *state);
 
 
 #ifdef __cplusplus
