ab64030ae08a52995aa8b6ed1c2162c6117aa5ed
Rylie Pavlik <rylie.pavlik@collabora.com>
2023-07-24 17:25:50 -0500

    Add log header

---
+++ client/src/gst/app_log.h
@@ -0,0 +1,12 @@
+// Copyright 2020-2023, Collabora, Ltd.
+// SPDX-License-Identifier: BSL-1.0
+
+#pragma once
+
+#include <android/log.h>
+
+#define ALOGV(...) __android_log_print(ANDROID_LOG_VERBOSE, "ElectricMaple", __VA_ARGS__)
+#define ALOGD(...) __android_log_print(ANDROID_LOG_DEBUG, "ElectricMaple", __VA_ARGS__)
+#define ALOGI(...) __android_log_print(ANDROID_LOG_INFO, "ElectricMaple", __VA_ARGS__)
+#define ALOGW(...) __android_log_print(ANDROID_LOG_WARN, "ElectricMaple", __VA_ARGS__)
+#define ALOGE(...) __android_log_print(ANDROID_LOG_ERROR, "ElectricMaple", __VA_ARGS__)
