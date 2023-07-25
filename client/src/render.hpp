87ff5cb6935948f214882c762d2e19aa5a4bbd00
Ryan Pavlik <ryan.pavlik@collabora.com>
2023-07-25 15:29:42 -0500

    Port stuff over and isolate changes

--- client/src/render.hpp
+++ client/src/render.hpp
@@ -1,8 +1,11 @@
 #include "gst/gst_common.h"
+#include <GLES3/gl3.h>
+
+struct em_state;
 
 void
-initializeEGL(struct state_t &state);
+initializeEGL(struct em_state &state);
 void
 setupRender();
 void
-draw(GLuint framebuffer, GLuint texture);
+draw(GLuint framebuffer, GLuint texture, GLenum texture_target);
