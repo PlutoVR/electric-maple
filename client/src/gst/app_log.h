21dc3906ed2f6bb8465fe939bf17c85ca3510978
Rylie Pavlik <rylie.pavlik@collabora.com>
2023-07-26 15:05:35 -0500

    adjust log header slightly

--- client/src/gst/app_log.h
+++ client/src/gst/app_log.h
@@ -5,8 +5,9 @@
 
 #include <android/log.h>
 
-#define ALOGV(...) __android_log_print(ANDROID_LOG_VERBOSE, "ElectricMaple", __VA_ARGS__)
-#define ALOGD(...) __android_log_print(ANDROID_LOG_DEBUG, "ElectricMaple", __VA_ARGS__)
-#define ALOGI(...) __android_log_print(ANDROID_LOG_INFO, "ElectricMaple", __VA_ARGS__)
-#define ALOGW(...) __android_log_print(ANDROID_LOG_WARN, "ElectricMaple", __VA_ARGS__)
-#define ALOGE(...) __android_log_print(ANDROID_LOG_ERROR, "ElectricMaple", __VA_ARGS__)
+#define LOG_TAG "ElectricMaple"
+#define ALOGV(...) __android_log_print(ANDROID_LOG_VERBOSE, LOG_TAG, __VA_ARGS__)
+#define ALOGD(...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
+#define ALOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+#define ALOGW(...) __android_log_print(ANDROID_LOG_WARN, LOG_TAG, __VA_ARGS__)
+#define ALOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
