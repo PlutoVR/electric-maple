1d2c516c3297d8e99ce3ee5d689677a23e92ebde
Ryan Pavlik <ryan.pavlik@collabora.com>
2023-08-03 16:20:32 -0500

    Port em_connection from monado-style to gobject

--- client/src/gst/em_connection.h
+++ client/src/gst/em_connection.h
@@ -4,15 +4,12 @@
  * @file
  * @brief  Internal header for the connection module of the ElectricMaple XR streaming solution
  * @author Ryan Pavlik <rpavlik@collabora.com>
- * @author Moshi Turner <moses@collabora.com>
- * @author Jakob Bornecrantz <jakob@collabora.com>
- * @author Christoph Haag <christoph.haag@collabora.com>
- * @author Pete Black <pblack@collabora.com>
- * @ingroup xrt_fs_em
+ * @ingroup em_client
  */
 #pragma once
-#include "em_status.h"
 
+
+#include <glib-object.h>
 #include <gst/gstelement.h>
 
 #define GST_USE_UNSTABLE_API
@@ -29,75 +26,49 @@ typedef GstElement *(*emconn_launch_pipeline_callback)(gpointer user_data);
 /// Type of a callback on connection drop/shutdown.
 typedef void (*emconn_drop_pipeline_callback)(gpointer user_data);
 
-/*!
- * Data required for the handshake to complete and to maintain the connection.
- */
-struct em_connection
-{
-	SoupSession *soup_session;
-	SoupWebsocketConnection *ws;
-	GstElement *webrtcbin;
-	gchar *websocket_uri;
-	GstWebRTCDataChannel *datachannel;
-	guint timeout_src_id;
-	guint retry_timeout_src_id;
 
-	enum em_status status;
-	/// has the pipeline been launched (@ref launch_pipeline called)
-	bool pipeline_launched;
+#define EM_TYPE_CONNECTION em_connection_get_type()
 
-	struct
-	{
-		emconn_launch_pipeline_callback callback;
-		gpointer data;
-	} launch_pipeline;
-
-	struct
-	{
-		emconn_drop_pipeline_callback callback;
-		gpointer data;
-	} drop_pipeline;
-};
+G_DECLARE_FINAL_TYPE(EmConnection, em_connection, EM, CONNECTION, GObject)
 
 /*!
- * Prep the handshake struct fields
+ * Create a connection object
  *
- * @param emconn self
  * @param launch_callback Function pointer to call to create a pipeline
  * @param drop_callback Function pointer to call when dropping a connection - must be idempotent
  * @param data Optional userdata to include when calling @p launch_callback and @p drop_callback
  * @param websocket_uri The websocket URI to connect to. Ownership does not transfer (we copy it)
  *
- * @memberof em_handshake
+ * @memberof EmConnection
  */
-void
-em_connection_init(struct em_connection *emconn,
-                   emconn_launch_pipeline_callback launch_callback,
-                   emconn_drop_pipeline_callback drop_callback,
-                   gpointer data,
-                   gchar *websocket_uri);
+EmConnection *
+em_connection_new(emconn_launch_pipeline_callback launch_callback,
+                  emconn_drop_pipeline_callback drop_callback,
+                  gpointer data,
+                  gchar *websocket_uri);
+
 
 /*!
  * Actually start connecting to the server
  */
 void
-em_connection_connect(struct em_connection *emconn);
+em_connection_connect(EmConnection *emconn);
 
 
 /*!
  * Drop the server connection, if any.
  */
 void
-em_connection_disconnect(struct em_connection *emconn);
+em_connection_disconnect(EmConnection *emconn);
 
 
 /*!
  * Send a message to the server
  */
 bool
-em_connection_send_bytes(struct em_connection *emconn, GBytes *bytes);
+em_connection_send_bytes(EmConnection *emconn, GBytes *bytes);
 
-/// Clean up the handshake struct fields
-/// @memberof em_handshake
-void
-em_connection_fini(struct em_connection *emconn);
+// /// Clean up the handshake struct fields
+// /// @memberof em_handshake
+// void
+// em_connection_fini(struct em_connection *emconn);
