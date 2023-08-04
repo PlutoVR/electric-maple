bf12222e4dcf57763304bf7d7ece519b80815f5e
Ryan Pavlik <ryan.pavlik@collabora.com>
2023-08-04 15:52:20 -0500

    Make partially buildable on desktop for gtkdoc

--- client/src/gst/app_log.h
+++ client/src/gst/app_log.h
@@ -3,11 +3,20 @@
 
 #pragma once
 
+#define LOG_TAG "ElectricMaple"
+
+#ifdef __ANDROID__
 #include <android/log.h>
 
-#define LOG_TAG "ElectricMaple"
 #define ALOGV(...) __android_log_print(ANDROID_LOG_VERBOSE, LOG_TAG, __VA_ARGS__)
 #define ALOGD(...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
 #define ALOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
 #define ALOGW(...) __android_log_print(ANDROID_LOG_WARN, LOG_TAG, __VA_ARGS__)
 #define ALOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+#else
+#define ALOGV(...)
+#define ALOGD(...)
+#define ALOGI(...)
+#define ALOGW(...)
+#define ALOGE(...)
+#endif
