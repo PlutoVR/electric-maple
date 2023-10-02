f1b16bf2c7a58c4816b5252cb2ff66fb93540e00
Ryan Pavlik <ryan.pavlik@collabora.com>
2023-10-02 10:16:59 -0500

    We do not need a modified version anymore, the upstreamed patches are in a release.

--- client/download_gst.sh
+++ client/download_gst.sh
@@ -7,9 +7,9 @@ cd $(dirname $0)
 mkdir -p ./deps/gstreamer_android
 
 # Download the file using curl
-curl -L -o ./deps/gstreamer_android/gstreamer-1.0-android-universal-1.22.3.tar.xz https://gstreamer.freedesktop.org/data/pkg/android/1.22.3/gstreamer-1.0-android-universal-1.22.3.tar.xz
+curl -L -o ./deps/gstreamer_android/gstreamer-1.0-android-universal-1.22.6.tar.xz https://gstreamer.freedesktop.org/data/pkg/android/1.22.6/gstreamer-1.0-android-universal-1.22.6.tar.xz
 
 # Extract the contents of the downloaded file to the destination directory
-tar -xf ./deps/gstreamer_android/gstreamer-1.0-android-universal-1.22.3.tar.xz -C ./deps/gstreamer_android
+tar -xf ./deps/gstreamer_android/gstreamer-1.0-android-universal-1.22.6.tar.xz -C ./deps/gstreamer_android
 
-rm ./deps/gstreamer_android/gstreamer-1.0-android-universal-1.22.3.tar.xz
+rm ./deps/gstreamer_android/gstreamer-1.0-android-universal-1.22.6.tar.xz
